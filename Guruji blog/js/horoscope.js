b9e0d86d510181fdd2f6a9fb0e1df988529acb8b
Mouri Roy <lognoroy2000@gmail.com>
2022-08-31 10:59:01 +0530

    before tarot

---
+++ Guruji blog/js/horoscope.js
@@ -0,0 +1,166 @@
+function googleTranslateElementInit() 
+    {
+      new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+    }
+
+
+
+    
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+
+// dropdown
+
+// let brownButton=document.getElementById("brown-button")
+
+// function colorfunc()
+// {
+//   brownButton.style.backgroundColor="#db5135";
+// }
+
+// let stayDiv=document.getElementById("stay-div")
+
+// function stayfunc()
+// {
+//   stayDiv.style.display="block";
+// }
+
+function myFunction() {
+  document.getElementById("dropdownMenuLink").classList.add("color-change");
+} 
+
+// accordition
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    // this.classList.toggle("active");
+    // var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } 
+    else if (panel.style.display === "none"){
+      panel.style.display = "block";
+    }
+  });
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+function myMenu()
+{
+  document.getElementById("click").classList.remove("active")
+  document.getElementById("all-item").classList.add("active")
+
+}
+function myBack()
+{
+  document.getElementById("click").classList.add("active")
+  document.getElementById("all-item").classList.remove("active")
+
+}
+function myCategories()
+{
+  document.getElementById("click2").classList.remove("active")
+  document.getElementById("click").classList.add("active")
+  document.getElementById("all-item").classList.add("active")
+}
+function myBack2()
+{
+  document.getElementById("click2").classList.add("active")
+  document.getElementById("all-item").classList.remove("active")
+
+}
+function myChat()
+{
+  document.getElementById("all-item").classList.add("active")
+  document.getElementById("click").classList.add("active")
+
+  document.getElementById("click2").classList.add("active")
+  document.getElementById("message").classList.remove("active")
+
+}
+
+
+
+
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+
+
---
+++ Guruji shop/js/payment.js
@@ -0,0 +1,131 @@
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+var sliderr = document.getElementById("myRanger");
+var outputr = document.getElementById("demor");
+outputr.innerHTML = sliderr.value;
+
+sliderr.oninput = function() {
+  outputr.innerHTML = this.value;
+}
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+$(document).ready(function() {
+
+  $('.color-choose input').on('click', function() {
+      var headphonesColor = $(this).attr('data-image');
+  
+      $('.active').removeClass('active');
+      $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+      $(this).addClass('active');
+  });
+  
+  });
+
+  function show(){
+    document.getElementById("youtube").style.display="none";
+    document.querySelector("main").classList.remove("active")
+  }
+ function show2(){
+  document.getElementById("youtube").style.display="none";
+  document.getElementById("iovory").classList.remove("active")
+ }
+  
+function incrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if(value<10){
+        value++;
+            document.getElementById('number').value = value;
+    }
+}
+function decrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if(value>1){
+        value--;
+            document.getElementById('number').value = value;
+    }
+
+}
+
+(function() {
+
+  function addSeparator(nStr) {
+      nStr += '';
+      var x = nStr.split('.');
+      var x1 = x[0];
+      var x2 = x.length > 1 ? '.' + x[1] : '';
+      var rgx = /(\d+)(\d{3})/;
+      while (rgx.test(x1)) {
+          x1 = x1.replace(rgx, '$1' + '.' + '$2');
+      }
+      return x1 + x2;
+  }
+
+  function rangeInputChangeEventHandler(e){
+      var rangeGroup = $(this).attr('name'),
+          minBtn = $(this).parent().children('.min'),
+          maxBtn = $(this).parent().children('.max'),
+          range_min = $(this).parent().children('.range_min'),
+          range_max = $(this).parent().children('.range_max'),
+          minVal = parseInt($(minBtn).val()),
+          maxVal = parseInt($(maxBtn).val()),
+          origin = $(this).context.className;
+
+      if(origin === 'min' && minVal > maxVal-5){
+          $(minBtn).val(maxVal-5);
+      }
+      var minVal = parseInt($(minBtn).val());
+      $(range_min).html(addSeparator(minVal*1000) + ' €');
+
+
+      if(origin === 'max' && maxVal-5 < minVal){
+          $(maxBtn).val(5+ minVal);
+      }
+      var maxVal = parseInt($(maxBtn).val());
+      $(range_max).html(addSeparator(maxVal*1000) + ' €');
+  }
+
+$('input[type="range"]').on( 'input', rangeInputChangeEventHandler);
+})();
+
+
+function openNav() {
+  
+  document.getElementById("mySidebar").style.width = "450px";
+  // document.getElementById("main").style.marginRight = "250px";
+  document.getElementById("button4").style.left="72.5%";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginRight= "0";
+}
+	
