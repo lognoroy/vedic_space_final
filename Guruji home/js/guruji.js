7f8c1e1043899b6843a4eef32f2bad0dd6a286be
Mouri Roy <lognoroy2000@gmail.com>
2022-08-31 19:48:48 +0530

    vedic

--- Guruji home/js/guruji.js
+++ Guruji home/js/guruji.js
@@ -20,37 +20,86 @@ function typeWriter() {
   typeWriter()
  
 
-//typing ends
-
-
-//accordian starts
-
-//accordian ends
 
 
 
-//sticky nav starts
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  function myMenu()
+  {
+    document.getElementById("click").classList.remove("active")
+    document.getElementById("all-item").classList.add("active")
+
+  }
+  function myBack()
+  {
+    document.getElementById("click").classList.add("active")
+    document.getElementById("all-item").classList.remove("active")
+
+  }
+  function myCategories()
+  {
+    document.getElementById("click2").classList.remove("active")
+    document.getElementById("click").classList.add("active")
+    document.getElementById("all-item").classList.add("active")
+  }
+  function myBack2()
+  {
+    document.getElementById("click2").classList.add("active")
+    document.getElementById("all-item").classList.remove("active")
+
+  }
+  function myChat()
+  {
+    document.getElementById("all-item").classList.add("active")
+    document.getElementById("click").classList.add("active")
+
+    document.getElementById("click2").classList.add("active")
+    document.getElementById("message").classList.remove("active")
+
+  }
 
 
-// window.addEventListener("scroll",function()
-// {
-// let elm=document.getElementById("menu-bar")
-
-//   if(window.pageYOffset>=30)
-//   {
-//     elm.classList.add("sticky")
-//   }
-//   else
-//   {
-//     elm.classList.remove("sticky")
-
-//   }
-// })
 
 
-//sticky nav ends
-
-
-
-
-
+  function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
